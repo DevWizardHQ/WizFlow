5dc98f9f0b3c93638cec168add9b1b2d6fa4176e
shazeedul <syedshazeedul@gmail.com>
2026-01-21 07:11:44 +0600

    update

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+import path from 'path';
+const __dirname = path.dirname(__filename);
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'babel-plugin-react-compiler',
+      'react-native-reanimated/plugin'
+    ],
+  };
+};
